b6e44a08edd0d98c41a0b9aa8dc44ce60a4dbfac
Neil Hu <hysh1991@gmail.com>
2020-05-26 21:50:11 +1000

    content security policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -147,14 +147,14 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt =>
+            app.UseCsp(opt =>
                 opt.BlockAllMixedContent()
-                .StyleSources(s => s.Self())
-                .FontSources(s => s.Self())
+                .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+                .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
                 .FormActions(s => s.Self())
                 .FrameAncestors(s => s.Self())
-                .ImageSources(s => s.Self())
-                .ScriptSources(s => s.Self())
+                .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:"))
+                .ScriptSources(s => s.Self().CustomSources("sha256-ma5XxS1EBgt17N22Qq31rOxxRWRfzUTQS1KOtfYwuNo="))
                 );
 
             // app.UseHttpsRedirection();
